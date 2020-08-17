81c2341ee826685473fed0355707e2bcc571fe4b
matteo ratti <matteoratti90@gmail.com>
2020-08-17 17:17:31 +0200

    linear change bg effect

--- speaker/index.js
+++ speaker/index.js
@@ -1,5 +1,6 @@
 function speak(id) {
   const message = document.getElementById(id).value;
+  const container = document.querySelector('.container');
 
   let speech = new SpeechSynthesisUtterance();
 
@@ -10,10 +11,27 @@ function speak(id) {
   speech.rate = 1;
   speech.pitch = 1;
 
+  container.style.backgroundPosition = "left bottom";
+
+  setTimeout(() => {
+    container.style.backgroundPosition = "right bottom";
+  },2000)
+
   window.speechSynthesis.speak(speech);
+
 }
 
 
 
 // http://www.lingoes.net/en/translator/langcode.html
 // https://mdn.github.io/web-speech-api/speak-easy-synthesis/
+
+
+
+/* 
+
+  setTimeout(() => {
+    container.style.backgroundPosition = "right bottom";
+  },1)
+
+*/
