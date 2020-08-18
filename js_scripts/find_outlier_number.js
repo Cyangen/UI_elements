998e782904b0fb079b7eda12f7c1d0e8ec1fcdd1
matteo ratti <matteoratti90@gmail.com>
2020-08-18 14:45:31 +0200

    add best practice

--- js_scripts/find_outlier_number.js
+++ js_scripts/find_outlier_number.js
@@ -28,3 +28,10 @@ function findOutlier(integers){
   
  return outlier;
 }
+
+//best practices
+function findOutlier(int){
+  var even = int.filter(a=>a%2==0);
+  var odd = int.filter(a=>a%2!==0);
+  return even.length==1? even[0] : odd[0];
+}
