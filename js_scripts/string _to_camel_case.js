4bb3468a6057a63de9f3649a5fa233b9aae136af
matteo ratti <matteoratti90@gmail.com>
2020-08-18 13:01:25 +0200

    improve script

--- js_scripts/string _to_camel_case.js
+++ js_scripts/string _to_camel_case.js
@@ -1,26 +1,3 @@
 function toCamelCase(str){
-  if(str === ""){ 
-    return ""
-  }
-  
-  if(str.includes("_")){
-    let splitStr = str.split("_");
-    
-    var newStr = splitStr.map(el =>{
-      return el.charAt(0).toUpperCase() + el.slice(1);
-    }).join("");
-        
-  }else if(str.includes("-")){
-    let splitStr = str.split("-");
-    
-    var newStr = splitStr.map(el =>{
-      return el.charAt(0).toUpperCase() + el.slice(1);
-    }).join("");
-  }
-  
-  if(str[0] !== str[0].toUpperCase()){
-    return newStr.charAt(0).toLowerCase() + newStr.slice(1)
-  }else {
-     return newStr;
-  }
+  return str.replace(/[-_](.)/g, (_, c) => c.toUpperCase());
 }
